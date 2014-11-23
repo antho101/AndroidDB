5aac7e9e1dfbc4803d9e314feaf8bf828b49c1c5
alexandre rosati <alex7170@gmail.com>
2014-11-23 20:07:46 +0100

    controller extra

--- src/testprojetandroid/TestProjetAndroid.java
+++ src/testprojetandroid/TestProjetAndroid.java
@@ -3,6 +3,7 @@ package testprojetandroid;
 public class TestProjetAndroid {
 
     protected Controler c = new Controler();
+    //protected Controler_antho c = new Controler_antho();
 
     public static void main(String[] args) {
         TestProjetAndroid projet = new TestProjetAndroid();
@@ -11,6 +12,7 @@ public class TestProjetAndroid {
     public TestProjetAndroid() {
 
         Controler c = new Controler();
+        //Controler_antho c = new Controler_antho();
         c.Menu();
     }
 
