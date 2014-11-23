6c2780ad4536396bb018cd50c63ec9e78ba19fff
alexandre rosati <alex7170@gmail.com>
2014-11-23 19:54:30 +0100

    Partie alex userdb & carnetdb + mvc

---
+++ src/testprojetandroid/TestProjetAndroid.java
@@ -0,0 +1,17 @@
+package testprojetandroid;
+
+public class TestProjetAndroid {
+
+    protected Controler c = new Controler();
+
+    public static void main(String[] args) {
+        TestProjetAndroid projet = new TestProjetAndroid();
+    }
+
+    public TestProjetAndroid() {
+
+        Controler c = new Controler();
+        c.Menu();
+    }
+
+}
